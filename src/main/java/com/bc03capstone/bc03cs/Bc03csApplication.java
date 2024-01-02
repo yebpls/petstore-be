58c936c7ab967f3e592cf58840899c0559d08a43
happyday <jsoibn@gmail.com>
2024-01-02 13:04:42 +0700

    first commit

---
+++ src/main/java/com/bc03capstone/bc03cs/Bc03csApplication.java
@@ -0,0 +1,13 @@
+package com.bc03capstone.bc03cs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bc03csApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bc03csApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bc03capstone/bc03cs/Bc03csApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bc03capstone.bc03cs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bc03csApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
