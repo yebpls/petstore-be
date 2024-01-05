1f6bacf423d960803ff5199d3f69ca18278284cb
AnhQuoc <anhquoc11358@gmail.com>
2024-01-05 15:43:43 +0900

    fix implements

--- src/main/java/com/bc03capstone/bc03cs/service/OrderItemService.java
+++ src/main/java/com/bc03capstone/bc03cs/service/OrderItemService.java
@@ -1,12 +1,13 @@
 package com.bc03capstone.bc03cs.service;
 
 import com.bc03capstone.bc03cs.repository.OrderItemRepository;
+import com.bc03capstone.bc03cs.service.imp.OrderItemServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
 
 @Service
-public class OrderItemService {
+public class OrderItemService implements OrderItemServiceImp {
     @Autowired
     private OrderItemRepository orderItemRepository;
 }
--- src/main/java/com/bc03capstone/bc03cs/service/SpeciesService.java
+++ src/main/java/com/bc03capstone/bc03cs/service/SpeciesService.java
@@ -1,11 +1,12 @@
 package com.bc03capstone.bc03cs.service;
 
 import com.bc03capstone.bc03cs.repository.SpeciesRepository;
+import com.bc03capstone.bc03cs.service.imp.SpeciesServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SpeciesService {
+public class SpeciesService implements SpeciesServiceImp {
 
     @Autowired
     private SpeciesRepository speciesRepository;
