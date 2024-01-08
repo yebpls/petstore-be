b118357fa56b4f9230226485e9278257427bfd77
happyday <jsoibn@gmail.com>
2024-01-08 17:02:17 +0700

    chores

--- src/main/java/com/bc03capstone/bc03cs/jwt/JwtHelper.java
+++ src/main/java/com/bc03capstone/bc03cs/jwt/JwtHelper.java
@@ -3,6 +3,7 @@ package com.bc03capstone.bc03cs.jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -15,12 +16,17 @@ public class JwtHelper {
     private String strKey;
 
     private long expiredTime = 60L*24*60*60*1000;
-    public String generateToken (String data) {
+    public String generateToken (String data, String roles, int id) {
         SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(strKey));
         Date date = new Date();
         long futureMilis = date.getTime() + expiredTime;
         Date futureDate = new Date(futureMilis);
-        return Jwts.builder().subject(data).expiration(futureDate).signWith(key).compact();
+        return Jwts.builder().setSubject(data)
+            .claim("roles", roles)
+            .claim("id", id)// Add roles as a claim
+            .setExpiration(futureDate)
+            .signWith(key)
+            .compact();
     }
 
     public String decodeToken(String token) {
